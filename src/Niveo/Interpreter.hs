85fc7645ee1843c962391ea49ed030cc17a432dd
rami3l <rami3l@outlook.com>
2022-09-27 10:28:52 +0800

    refactor(runtime): introduce `effectful`

--- src/Niveo/Interpreter.hs
+++ src/Niveo/Interpreter.hs
@@ -1,16 +1,18 @@
 module Niveo.Interpreter where
 
-import Control.Monad.Trans.Except (except, mapExceptT, throwE)
 import Data.Default (Default (def))
 import Data.Either.Extra (mapLeft, maybeToEither)
 import Data.String.Interpolate
 import Data.Vector (Vector)
+import Effectful
+import Effectful.Error.Static
+import Effectful.State.Static.Local
 import Error.Diagnose (Marker (This), addFile, addReport, err)
 import Error.Diagnose.Diagnostic (Diagnostic)
 import Error.Diagnose.Report (Report)
 import Niveo.Parser (Expr (..), Prog (..), Token (..), TokenType (..), parse, program)
 import Optics.TH (makeFieldLabelsNoPrefix)
-import Relude
+import Relude hiding (State, get)
 
 data Val
   = -- Native JSON types.
@@ -35,27 +37,36 @@ data Context = Context {env :: Env, fin :: FilePath, src :: Text}
 
 makeFieldLabelsNoPrefix ''Context
 
-interpret :: ExceptT (Diagnostic Text) (State Context) Val
+interpret ::
+  (Error (Diagnostic Text) :> es, State Context :> es) =>
+  Eff es Val
 interpret = do
-  ctx <- get
-  prog <- except $ parse program ctx.fin ctx.src
-  let diag = def `addFile` ctx.fin $ toString ctx.src
-  eval prog.expr & mapExceptT (<&> mapLeft (diag `addReport`))
+  ctx <- get @Context
+  prog <- parse program ctx.fin ctx.src & either throwError pure
+  eval prog.expr
 
-eval :: Expr -> ExceptT (Report Text) (State Context) Val
-eval (EUnary op rhs) = do
-  rhs' <- eval rhs
-  case (op.type_, rhs') of
-    (TBang, VBool b) -> pure . VBool $ not b
-    (TPlus, VNum n) -> pure . VNum $ n
-    (TMinus, VNum n) -> pure . VNum $ negate n
-    _ ->
-      throwE $
-        err
-          Nothing
-          "Type mismatch"
-          [(op.range, This [i|Could not apply `#{op}` to `#{rhs'}`|])]
-          []
+eval ::
+  (Error (Diagnostic Text) :> es, State Context :> es) =>
+  Expr ->
+  Eff es Val
+eval expr = do
+  ctx <- get @Context
+  let diag = addFile @Text def ctx.fin $ toString ctx.src
+  throwError diag
+
+-- eval (EUnary op rhs) = do
+--   rhs' <- eval rhs
+--   case (op.type_, rhs') of
+--     (TBang, VBool b) -> pure . VBool $ not b
+--     (TPlus, VNum n) -> pure . VNum $ n
+--     (TMinus, VNum n) -> pure . VNum $ negate n
+--     _ ->
+--       throwE $
+--         err
+--           Nothing
+--           "Type mismatch"
+--           [(op.range, This [i|Could not apply `#{op}` to `#{rhs'}`|])]
+--           []
 
 -- eval (EVar Token {lexeme}) = do
 --   def <- get <&> (Map.!? lexeme)
