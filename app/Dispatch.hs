b287ee793fdcf49976279099ac2f13f9815f93fb
rami3l <rami3l@outlook.com>
2022-10-19 12:59:46 +0800

    chore: clean up code even more, pt. 2

--- app/Dispatch.hs
+++ app/Dispatch.hs
@@ -8,7 +8,7 @@ import Data.Default (Default (..))
 import Effectful
 import Effectful.Error.Static
 import Effectful.Reader.Static
-import Error.Diagnose.Diagnostic (Diagnostic, printDiagnostic)
+import Error.Diagnose (Diagnostic, printDiagnostic)
 import Error.Diagnose.Style (defaultStyle)
 import Niveo.Interpreter (Context (..), Val, evalTxt, throwReport)
 import Niveo.Interpreter.FileSystem (FsError (FsError), runFileSystemIO)
@@ -43,25 +43,27 @@ args = Args <$> optional (strOption load)
         <> help "Load a source file"
 
 dispatch :: Args -> IO ()
-dispatch (Args {fin}) = fin & maybe repl (runInputT defaultSettings . evalTxtInput)
-
-repl :: IO ()
-repl = runInputT defaultSettings $ fix \loop -> do
-  getInputLine ">> " >>= (`whenJust` evalTxtInput)
-  loop
-
-evalTxtInput :: MonadIO m => String -> InputT m ()
-evalTxtInput ln =
-  runEff evalTxtIO & liftIO >>= either printErr print'
+dispatch (Args {fin}) = fin & maybe repl runFile
   where
-    evalTxtIO :: Eff '[IOE] (Either (Diagnostic Text) Val) =
-      evalTxt
-        & runFileSystemIO
-        & runErrorNoCallStack @FsError
-        >>= either (\(FsError e) -> throwReport e []) pure
-        & runErrorNoCallStack @(Diagnostic _)
-        & runReader ctx
-    ctx = Context {env = def, fin = "<stdin>", src = toText ln}
-    print' = outputStrLn . ("<< " <>) . show @String
-    printErr = printDiagnostic stdout useUnicode useColors 4 defaultStyle
-    (useUnicode, useColors) = (True, True)
+    runInputT' = runInputT defaultSettings
+    runFile = runInputT' . evalTxtInput
+
+    repl = runInputT' $ fix \loop -> do
+      getInputLine ">> " >>= (`whenJust` evalTxtInput)
+      loop
+
+    evalTxtInput :: MonadIO m => String -> InputT m ()
+    evalTxtInput ln =
+      runEff evalTxtIO & liftIO >>= either printErr print'
+      where
+        evalTxtIO :: Eff '[IOE] (Either (Diagnostic Text) Val) =
+          evalTxt
+            & runFileSystemIO
+            & runErrorNoCallStack @FsError
+            >>= either (\(FsError e) -> throwReport e []) pure
+            & runErrorNoCallStack @(Diagnostic _)
+            & runReader ctx
+        ctx = Context {env = def, fin = "<stdin>", src = toText ln}
+        print' = outputStrLn . ("<< " <>) . show @String
+        printErr = printDiagnostic stdout useUnicode useColors 4 defaultStyle
+        (useUnicode, useColors) = (True, True)
