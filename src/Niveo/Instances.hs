ebf848b19797a8d9bcc61d6b1d803123550bbb83
rami3l <rami3l@outlook.com>
2022-10-19 22:53:18 +0800

    feat(runtime): (wip) impl JSON import/export for `Val`

--- src/Niveo/Instances.hs
+++ src/Niveo/Instances.hs
@@ -2,7 +2,12 @@
 
 module Niveo.Instances () where
 
+import Data.Vector (Vector)
 import Error.Diagnose.Compat.Megaparsec (HasHints (..))
+import Witch
 
 instance {-# OVERLAPPABLE #-} HasHints Void msg where
   hints _ = mempty
+
+instance From (Vector a) (Seq a) where
+  from = fromList . toList
