dd92f9c71f5c8ed0a03b10ac9495be32c4cc4250
rami3l <rami3l@outlook.com>
2022-10-20 18:30:47 +0800

    feat(app): impl JSON import/export for `Val`, pt. 2

--- app/Dispatch.hs
+++ app/Dispatch.hs
@@ -4,7 +4,11 @@ module Dispatch
   )
 where
 
+import Control.Applicative.Combinators (choice)
+import Control.Monad.Catch (MonadCatch, catch)
+import Data.Aeson qualified as Aeson
 import Data.Default (Default (..))
+import Data.String.Interpolate
 import Effectful
 import Effectful.Error.Static
 import Effectful.Reader.Static
@@ -12,8 +16,11 @@ import Error.Diagnose (Diagnostic, printDiagnostic)
 import Error.Diagnose.Style (defaultStyle)
 import Niveo.Interpreter (Context (..), Val, evalTxt, throwReport)
 import Niveo.Interpreter.FileSystem (FsError (FsError), runFileSystemIO)
+import Niveo.Interpreter.Types ()
+import Niveo.Interpreter.Utils (diagnosticFromCtx)
 import Options.Applicative
   ( Parser,
+    flag',
     help,
     long,
     metavar,
@@ -27,36 +34,27 @@ import System.Console.Haskeline
     outputStrLn,
     runInputT,
   )
+import UnliftIO (StringException (StringException), fromEither, stringException)
+import Witch
 import Prelude hiding (Reader, runReader)
 
-newtype Args = Args
-  { fin :: Maybe FilePath
-  }
-
-args :: Parser Args
-args = Args <$> optional (strOption load)
-  where
-    load =
-      long "load"
-        <> short 'f'
-        <> metavar "FILE"
-        <> help "Load a source file"
-
 dispatch :: Args -> IO ()
-dispatch (Args {fin}) = fin & maybe repl runFile
+dispatch a = case a.mode of
+  ALoad fin -> runTxt . decodeUtf8 =<< readFileBS fin
+  ACmd txt -> runTxt txt
+  AREPL -> repl
   where
     runInputT' = runInputT defaultSettings
-    runFile = runInputT' . evalTxtInput
+    runTxt = runInputT' . evalStrInput
 
     repl = runInputT' $ fix \loop -> do
-      getInputLine ">> " >>= (`whenJust` evalTxtInput)
+      getInputLine ">> " >>= (`whenJust` evalStrInput)
       loop
 
-    evalTxtInput :: MonadIO m => String -> InputT m ()
-    evalTxtInput ln =
-      runEff evalTxtIO & liftIO >>= either printErr print'
+    evalStrInput :: (MonadIO m, MonadCatch m) => String -> InputT m ()
+    evalStrInput ln = runEff evalStrIO & liftIO >>= either printErr print'
       where
-        evalTxtIO :: Eff '[IOE] (Either (Diagnostic Text) Val) =
+        evalStrIO :: Eff '[IOE] (Either (Diagnostic Text) Val) =
           evalTxt
             & runFileSystemIO
             & runErrorNoCallStack @FsError
@@ -64,6 +62,60 @@ dispatch (Args {fin}) = fin & maybe repl runFile
             & runErrorNoCallStack @(Diagnostic _)
             & runReader ctx
         ctx = Context {env = def, fin = "<stdin>", src = toText ln}
-        print' = outputStrLn . ("<< " <>) . show @String
-        printErr = printDiagnostic stdout useUnicode useColors 4 defaultStyle
+
+        print' v = do
+          when isREPL . outputStrLn $ "<< " <> show @String v
+          case a.exportFmt of
+            Nothing -> unless isREPL . outputStrLn $ show @String v
+            Just JSON ->
+              (intoAeson v >>= putLBSLn . Aeson.encode)
+                `catch` \(StringException s _) -> do
+                  printErr $ diagnosticFromCtx ctx (into s) []
+                  unless isREPL exitFailure
+        intoAeson =
+          fromEither
+            . first (\(TryFromException v _) -> stringException [i|value cannot be exported: #{v}|])
+            . tryInto @Aeson.Value
+        isREPL = a.mode == AREPL
+
+        printErr = printDiagnostic stderr useUnicode useColors 4 defaultStyle
         (useUnicode, useColors) = (True, True)
+
+data Args = Args
+  { mode :: AMode,
+    exportFmt :: Maybe ExportFormat
+  }
+
+args :: Parser Args
+args = Args <$> mode <*> optional exportFmt
+
+data AMode
+  = ALoad FilePath
+  | ACmd String
+  | AREPL
+  deriving (Eq)
+
+mode :: Parser AMode
+mode =
+  choice
+    [ ALoad
+        <$> strOption
+          ( long "run"
+              <> short 'f'
+              <> metavar "FILE"
+              <> help "Load and interpret FILE"
+          ),
+      ACmd
+        <$> strOption
+          ( long "cmd"
+              <> short 'c'
+              <> metavar "SCRIPT"
+              <> help "Interpret the given SCRIPT string"
+          ),
+      pure AREPL
+    ]
+
+data ExportFormat = JSON
+
+exportFmt :: Parser ExportFormat
+exportFmt = flag' JSON (long "export-json" <> long "json" <> help "Enable JSON output")
